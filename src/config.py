d413748ca55afe1ef4595d455e831ca576b50297
HaxerWaddle <bucnghivi@gmail.com>
2025-02-27 20:51:26 +0700

    STUPID TYPO

--- src/config.py
+++ src/config.py
@@ -5,6 +5,6 @@ import os
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABSE_URL')
+    SECRET_KEY = os.getenv('SECRET_KEY', )
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     REMEMBER_COOKIE_DURATION = timedelta(days=1)
